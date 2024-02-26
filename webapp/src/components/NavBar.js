7112910c900f5b6986ffb7431672335b95f1b184
bidof <uo283196@uniovi.es>
2024-02-27 00:03:31 +0100

    agregar sistema de inicio de sesion y modifiacion de la navbar

--- webapp/src/components/NavBar.js
+++ webapp/src/components/NavBar.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React ,{useContext} from 'react';
 import '../styles/Navbar.css';
 import { Link } from 'react-router-dom';
+import { AuthContext } from './authcontext';
 
-const Navbar = () => (
+
+const Navbar = () => {
+  //sacar el valor para ver si esta logeado o no 
+  const { isLoggedIn } = useContext(AuthContext);
+
+  return(
   <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
     <div className="collapse navbar-collapse" id="my-navbarColor01">
       <Link to="/" className="navbar-brand">
@@ -11,22 +17,44 @@ const Navbar = () => (
     </div>
 
     <div className="collapse navbar-collapse" id="my-navbarColor02">
-      <ul className="navbar-nav justify-content-end">
-        <li className="nav-item">
-          <Link to="/addUser" className="nav-link">
-            <i className="fas fa-sign-in-alt" style={{ fontSize: '16px' }}></i>
-            <span>Registrarse</span>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <a href="/login" className="nav-link">
-            <i className="fas fa-sign-in-alt" style={{ fontSize: '16px' }}></i>
-            <span>Iniciar sesión</span>
-          </a>
-        </li>
-      </ul>
-    </div>
+        <ul className="navbar-nav justify-content-end">
+          {!isLoggedIn ? (
+            <>
+              <li className="nav-item">
+                <Link to="/addUser" className="nav-link">
+                  <i className="fas fa-sign-in-alt" style={{ fontSize: '16px' }}></i>
+                  <span>Registrarse</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/login" className="nav-link">
+                  <i className="fas fa-sign-in-alt" style={{ fontSize: '16px' }}></i>
+                  <span>Iniciar sesión</span>
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+            <li className="nav-item">
+              <Link to="/logout" className="nav-link">
+                <i className="fas fa-sign-in-alt" style={{ fontSize: '16px' }}></i>
+                <span>logout</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/history" className="nav-link">
+                <i className="fas fa-sign-in-alt" style={{ fontSize: '16px' }}></i>
+                <span>historial</span>
+              </Link>
+            </li>
+            </>
+
+            
+          )}
+        </ul>
+      </div>
   </nav>
-);
+  );
+          };
 
 export default Navbar;
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -16,9 +16,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
+
+    
     <AuthProvider>
     <Router>
       <Navbar />
+      <Typography component="h1" variant="h5" align="center" sx={{ marginTop: 2 }}>
+       Bienvenidos al curso 2024 de arquitectura de software somos el grupo :
+      </Typography>
+      <Typography component="h2" variant="h5" align="center" sx={{ marginTop: 2 }}>wiq_es04c</Typography>
         <Routes>
           <Route path="/addUser" element={<AddUser />} />
           <Route path="/login" element={<Login />} />
@@ -26,10 +32,7 @@ root.render(
       </Router>
 
       <CssBaseline />
-      <Typography component="h1" variant="h5" align="center" sx={{ marginTop: 2 }}>
-       Bienvenidos al curso 2024 de arquitectura de software somos el grupo :
-      </Typography>
-      <Typography component="h2" variant="h5" align="center" sx={{ marginTop: 2 }}>wiq_es04c</Typography>
+     
 
       <App />
       <Footer />
